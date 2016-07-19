e374b527e0243a8e598aacdc9b4de63f58456373
Ainor Syahrizal <ainorsyahrizal@ainors-macbook-pro.local>
2016-07-19 08:28:29 +0800

    Initial Commit

---
+++ ParkListingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkListingApp
+//
+//  Created by Ainor Syahrizal on 19/07/2016.
+//  Copyright © 2016 Ainor Syahrizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ParkListingAppTests/ParkListingAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParkListingAppTests.swift
+//  ParkListingAppTests
+//
+//  Created by Ainor Syahrizal on 19/07/2016.
+//  Copyright © 2016 Ainor Syahrizal. All rights reserved.
+//
+
+import XCTest
+@testable import ParkListingApp
+
+class ParkListingAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
