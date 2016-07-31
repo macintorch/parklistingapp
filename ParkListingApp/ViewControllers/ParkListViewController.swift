058a79014f33817f6246928f7b68d0c26d6c21c9
Ainor Syahrizal <ainorsyahrizal@Ainors-MacBook-Pro.local>
2016-07-31 16:21:22 +0800

    remove preloading data function

--- ParkListingApp/ViewControllers/ParkListViewController.swift
+++ ParkListingApp/ViewControllers/ParkListViewController.swift
@@ -24,7 +24,6 @@ class ParkListViewController: UIViewController {
     }
     
     
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
