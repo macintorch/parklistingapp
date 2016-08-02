01f1fb7dadb0b01fb1687054184be2972c591f30
Ainor Syahrizal <ainorsyahrizal@ainors-macbook-pro.local>
2016-08-03 07:44:48 +0800

    refactor session config and testJSONDataFromAPI function

--- ParkListingAppTests/ParkListingAppTests.swift
+++ ParkListingAppTests/ParkListingAppTests.swift
@@ -9,11 +9,19 @@
 import XCTest
 @testable import ParkListingApp
 
+
 class ParkListingAppTests: XCTestCase {
+    
+    var session: NSURLSession!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let config: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        config.HTTPAdditionalHeaders = ["X-Parse-Application-Id": "xAQEz3WeqfNFDUrhwex0fhErS1dJkAPFa6PkdKMz", "X-Parse-REST-API-Key": "hp7HZgv18V3QoSUIUPy4YQaeCWMLFUqQH3Z85ulk", "Content-Type": "application/json"]
+        
+        self.session = NSURLSession(configuration: config)
     }
     
     override func tearDown() {
@@ -47,14 +55,54 @@ class ParkListingAppTests: XCTestCase {
     func testLoadingAPI() {
         let expectation = self.expectationWithDescription("Load Task")
         
-        let session: NSURLSession = NSURLSession.sharedSession()
-        let url: NSURL = NSURL(string: "https://api.parse.com")!
+        let config: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        config.HTTPAdditionalHeaders = ["X-Parse-Application-Id": "xAQEz3WeqfNFDUrhwex0fhErS1dJkAPFa6PkdKMz", "X-Parse-REST-API-Key": "hp7HZgv18V3QoSUIUPy4YQaeCWMLFUqQH3Z85ulk", "Content-Type": "application/json"]
+        
+        let session = NSURLSession(configuration: config)
+        let url: NSURL = NSURL(string: "https://api.parse.com/1/classes/Park")!
         
         let loadTask: NSURLSessionDataTask = session.dataTaskWithURL(url) {
             (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
             //code
             
             NSLog("task completed")
+            expectation.fulfill()
+        }
+        
+        loadTask.resume()
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+    }
+    
+    func testJSONDataFromAPI() {
+        let expectation = self.expectationWithDescription("Load Task")
+        
+        
+        let url: NSURL = NSURL(string: "https://api.parse.com/1/classes/Park")!
+        
+        let loadTask: NSURLSessionDataTask = self.session.dataTaskWithURL(url) {
+            (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            //code
+            
+            let httpCode: Int = (response as! NSHTTPURLResponse).statusCode
+            
+            XCTAssert(httpCode == 200, "Server should have returned 200 content OK")
+            
+            var parks: [Park] = []
+            let json : [String : AnyObject] = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions(rawValue: 0)) as! [String : AnyObject]
+            
+             if let resultsArray: [ [String : AnyObject]] = json["results"] as? [ [String : AnyObject]] {
+                for jsonData in resultsArray {
+                    
+                    let name: String = jsonData["name"] as! String
+                    let objectId: String = jsonData["objectId"] as! String
+                    let park: Park = Park(name: name)!
+                    parks.append(park)
+                }
+            }
+            
+            XCTAssert(parks.count > 0, "Parks array shouldn't be empty")
+            
             expectation.fulfill()
         }
         
